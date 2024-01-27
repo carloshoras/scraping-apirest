9dfb4637f35e2922ba5068d786a5560a7b03bf94
carloshoras <carloshoras>
2024-01-27 17:08:01 +0100

    Ejercicio Terminado

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const fs = require('fs');
 const { router, guardarDatos } = require('./scraping')
 const app = express()
+
 let noticias
 
 app.use(express.json());
@@ -40,23 +41,30 @@ app.post('/', (req, res) => {
         descripcion: req.body.descripcion || '',
         enlace: req.body.enlace || ''
     }
-
     noticias.push(nuevaNoticia)
     guardarDatos(noticias)
-    
+})
+
+app.put('/:index', (req, res) => {
+    const index = req.params.index
+    leerDatos()
+    noticias[index] = {
+        titulo: req.body.titulo || noticias[index].titulo,
+        img: req.body.img || noticias[index].img,
+        descripcion: req.body.descripcion || noticias[index].descripcion,
+        enlace: req.body.enlace || noticias[index].enlace
+    }
+    guardarDatos(noticias)
 })
 
 app.delete('/:index', (req, res) => {
     const index = req.params.index
     leerDatos()
-
-    noticias.splice(index, 1)
-
-
-    res.json(noticias[index])
-    res.send(noticias[index])
+    const noticiaEliminada = noticias.splice(index, 1)
+    guardarDatos(noticias)
+    res.json(noticiaEliminada[0])
+    res.send(noticiaEliminada[0])
 })
-
 
 app.listen(3000, () => {
     console.log('Express está escuchando en el puerto http://localhost:3000/')
--- scraping.js
+++ scraping.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+
 const url = 'https://elpais.com/ultimas-noticias/'
 
 
